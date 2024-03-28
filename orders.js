150294f7890595090e35bc09334d8982f82a4a43
Renaldo Petlim <renaldopetlim@gmail.com>
2024-03-27 21:18:16 -0300

    Add Orders.js

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Encordoamento D`Adarrio EXL110-B 010-046',
+        productNumber: '000001',
+        paymentStatus: 'Devido',
+        status: 'Pendente'
+    },
+    {
+        productName: 'Encordoamento SG Hybrid Nickel Wound 010-049',
+        productNumber: '000002',
+        paymentStatus: 'Devolvido',
+        status: 'Recusado'
+    },
+    {
+        productName: 'Encordoamento Ernie Ball Regular Slinky 2221 010-046',
+        productNumber: '000003',
+        paymentStatus: 'Pago',
+        status: 'Ativo'
+    },
+]
